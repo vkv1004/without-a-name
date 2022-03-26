037ac3b6f0305af5d6c1bb0c85a580400285dd0e
Retr0-46 <88389726+Retr0-46@users.noreply.github.com>
2022-03-26 10:21:13 +0300

    Update main.py
    новые мегафункции были добавлены

--- main.py
+++ main.py
@@ -8,6 +8,31 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QPainter, QColor, QPixmap, QFont
 from PyQt5 import QtCore
 
+from pdfminer.high_level import extract_text
+import docx
+
+def save_pdf_in_txt(path):
+    txt = extract_text(path)
+
+    print(txt)
+
+    with open('file.txt', 'w+') as file:
+        file.write(txt)
+        file.close()
+
+
+def save_pdf_in_word(path):
+    txt = extract_text(path)
+
+    print(txt)
+
+    doc = docx.Document()
+    par1 = doc.add_paragraph('Импортированно из pdf')
+
+    par1.add_run(txt)
+
+    doc.save('sample.docx')
+
 class Example(QWidget):
     def __init__(self):
         super().__init__()
